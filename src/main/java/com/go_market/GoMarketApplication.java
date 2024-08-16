811bda53a99e402ed825407f060fcb3e3c879cf1
unknown <geovanyojeda14@gmail.com>
2024-08-15 21:02:54 -0500

    firts commit

---
+++ src/main/java/com/go_market/GoMarketApplication.java
@@ -0,0 +1,13 @@
+package com.go_market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoMarketApplication.class, args);
+	}
+
+}
